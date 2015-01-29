113d0fe0fcf923aded385d5fbef96fdd9583ac83
sointdx <sointdx@users.noreply.github.com>
2015-01-29 20:26:57 +0800

    fix:update may override setting of desktop background

--- customization-scripts/tdx/files/desktop/desktop.sh
+++ customization-scripts/tdx/files/desktop/desktop.sh
@@ -7,7 +7,7 @@ cp -a .config .gtkrc-2.0 .local 桌面 $HOME_DIR/
 #以root身份打开文件夹时需要设置文件管理器默认的长宽
 mkdir -p /root/.config
 cp -a .config /root/
-cp xfce-blue.jpg /usr/share/backgrounds/xfce/xfce-blue.jpg
+cp light-blue.jpg /usr/share/backgrounds/xfce/light-blue.jpg
 cp xfce4-whiskermenu-plugin.mo /usr/share/locale/zh_CN/LC_MESSAGES/
 rm $HOME_DIR/examples.desktop
 cd ../..
