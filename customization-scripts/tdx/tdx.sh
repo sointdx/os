7a03e215432bc7155e4e5a157c4e9e4fc94d6941
sointdx <sointdx@gmail.com>
2015-02-26 19:29:04 +0800

    improve annotate

--- customization-scripts/tdx/tdx.sh
+++ customization-scripts/tdx/tdx.sh
@@ -18,7 +18,7 @@ export WD_DIR=/tmp/customization-scripts/tdx
 # backports：backports 的团队则认为最好的更新策略是 security 策略加上新版本的软件（包括候选版本的）。但不会由Ubuntu security team审查和更新。
 # update：修复严重但不影响系统安全运行的漏洞，这类补丁在经过QA人员记录和验证后才提供，和security那类一样低风险。
 # proposed：update类的测试部分，仅建议提供测试和反馈的人进行安装。
-#xf
+#
 # 个人认为：
 # 1.重要的服务器：用发行版默认的、security 
 # 2.当有要较新软件包才行能运作的服务器：用发行版默认的、 security、（backports 还是不适合） 
